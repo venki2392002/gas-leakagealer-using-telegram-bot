5cd8774a20d606b0ee8e8773135a9926bd000486
venki2392002 <89253553+venki2392002@users.noreply.github.com>
2023-08-26 20:23:56 +0530

    Add files via upload

---
+++ telebot.ino
@@ -0,0 +1,58 @@
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+
+#include <UniversalTelegramBot.h>
+
+#define wifi_ssid "SPUNKY"
+#define wifi_password "8220668139"
+#define telegram_bot_token "6080620345:AAFzhohHgnkwgdpYUjBQhYjOJu4xTFVn_ag"
+#define telegram_chat_id "895600016"
+#define co_sensor_pin A0 // MQ-9 CO sensor connected to analog pin A0
+//#define ch4_sensor_pin A1 // MQ-9 CH4 sensor connected to analog pin A1
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(telegram_bot_token, client);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(co_sensor_pin, INPUT);
+  //pinMode(ch4_sensor_pin, INPUT);
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("WiFi connected!");
+}
+
+void loop() {
+  int co_level = analogRead(co_sensor_pin); // read CO level from sensor
+  //int ch4_level = analogRead(ch4_sensor_pin); // read CH4 level from sensor
+  Serial.print("CO Level: ");
+  Serial.print(co_level);
+  //Serial.print(" | CH4 Level: ");
+  //Serial.println(ch4_level);
+
+  if (co_level > 100) { // threshold for dangerous CO or CH4 level
+    sendAlert();
+    delay(60000); // wait for 1 minute before sending another notification
+  }
+
+  delay(1000); // wait for 1 second before checking again
+}
+
+void sendAlert() {
+  String message = "Dangerous gas levels detected!";
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Sending notification...");
+    if (bot.sendMessage(telegram_chat_id, message)) {
+      Serial.println("Telegram notification sent successfully!");
+    } else {
+      Serial.println("Failed to send Telegram notification!");
+    }
+  } else {
+    Serial.println("WiFi not connected, cannot send notification.");
+  }
+}
